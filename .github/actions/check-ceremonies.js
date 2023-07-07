2933d349712046b82405aead14d50da566e345e3
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-07-07 14:02:00 +0100

    chore(ceremonies) - add working r1cs and wasm and change path in workflow

--- .github/actions/check-ceremonies.js
+++ .github/actions/check-ceremonies.js
@@ -10,15 +10,15 @@ fs.readdir(ceremoniesPath, (err, folders) => {
   }
 
   folders.forEach(folder => {
-    const configPath = path.join(ceremoniesPath, folder, 'config.json');
+    const configPath = path.join(ceremoniesPath, folder, 'p0tionConfig.json');
 
     fs.readFile(configPath, 'utf-8', (err, data) => {
       if (err) {
-        console.error(`Failed to read config.json for ceremony "${folder}":`, err);
+        console.error(`Failed to read p0tionConfig.json for ceremony "${folder}":`, err);
         return;
       }
 
-      console.log(`config.json for ceremony "${folder}":\n`, data);
+      console.log(`p0tionConfig.json for ceremony "${folder}":\n`, data);
       //TODO: call CLI or page devs
     });
   });

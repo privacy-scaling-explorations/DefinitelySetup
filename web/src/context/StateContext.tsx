2a0bb14003678919fa49096834bc6c8bb921e59c
daodesigner <58278045+daodesigner@users.noreply.github.com>
2023-07-12 10:15:34 -0700

    React Context API integration (#26)
    * Squash and merge (#17)
    
    * Add check-ceremonies GitHub Action
    
    * test github action
    
    * responsive styles
    
    * feat: add firebase package, service configuration and utilities
    
    * chore: add .env to ignore
    
    * feat: add custom interfaces for p0tion firebase documents
    
    * refactor: organize mocked project object to support p0tion data
    
    * refactor: fetch ceremony data
    
    * refactor: grab data from circuits, participants and contributions
    
    * fix: wrong array update lead to missing contributions data
    
    * refactor: add direct link download button for final contribution and update one-command for contribution
    
    * chore(config) - add updated p0tionconfig.json files
    
    * chore(ceremonies) - add working r1cs and wasm and change path in workflow
    
    * chore(ceremonies) - re upload sample r1cs and wasm to ceremonies/example
    
    * Update issue templates
    
    * Create README.md
    
    * Update README.md
    
    * Create pull_request_template.md
    
    ---------
    
    Co-authored-by: Jeeiii <giacomo.corrias7@gmail.com>
    Co-authored-by: ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
    
    * feat : finish API integration, LandingPage Context, clean up data
    
    ---------
    
    Co-authored-by: Jeeiii <giacomo.corrias7@gmail.com>
    Co-authored-by: ctrlc03 <93448202+ctrlc03@users.noreply.github.com>

--- web/src/context/StateContext.tsx
+++ web/src/context/StateContext.tsx
@@ -1,6 +1,6 @@
 // useStateContext.tsx
 
-import React, { useState, createContext, useEffect } from "react";
+import React, { useState, createContext, useEffect, useContext } from "react";
 import { CeremonyDocumentReferenceAndData, CeremonyState, CeremonyTimeoutType, CeremonyType, CircuitDocumentReferenceAndData, ContributionDocumentReferenceAndData, ParticipantDocumentReferenceAndData } from "../helpers/interfaces";
 import { getAllCollectionDocs, initializeFirebaseCoreServices } from "../helpers/utils";
 import { DocumentData } from 'firebase/firestore'
@@ -21,12 +21,13 @@ export interface State {
   setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+
 export const StateContext = createContext<State>({
   projects: [
     // Initial project data
     {
       ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
+        uid: "A8CVrp2MMx7KO512KFdv",
         data: {
           title: "example",
           prefix: "example",
@@ -53,187 +54,6 @@ export const StateContext = createContext<State>({
 export const useInitialStateContext = () => {
   const [projects, setProjects] = useState<Project[]>([
     // Initial project data
-    {
-      ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
-        data: {
-          title: "@semaphore-protocol-example",
-          prefix: "example",
-          description: "This is an example ceremony",
-          startDate: new Date("2023-07-01").getTime(),
-          endDate: new Date("2023-07-31").getTime(),
-          timeoutMechanismType: CeremonyTimeoutType.FIXED,
-          penalty: 3600,
-          state: CeremonyState.OPENED,
-          type: CeremonyType.PHASE2,
-          coordinatorId: "uKm6XEjOKoeZUKAf2goY4vamgHE4",
-          lastUpdated: Date.now()
-        }
-      }
-    },
-    {
-      ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
-        data: {
-          title: "@maci-protocol-example",
-          prefix: "example",
-          description: "This is an example ceremony",
-          startDate: new Date("2023-07-01").getTime(),
-          endDate: new Date("2023-07-31").getTime(),
-          timeoutMechanismType: CeremonyTimeoutType.FIXED,
-          penalty: 3600,
-          state: CeremonyState.OPENED,
-          type: CeremonyType.PHASE2,
-          coordinatorId: "uKm6XEjOKoeZUKAf2goY4vamgHE4",
-          lastUpdated: Date.now()
-        }
-      }
-    },
-    {
-      ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
-        data: {
-          title: "@rln-example",
-          prefix: "example",
-          description: "This is an example ceremony",
-          startDate: new Date("2023-07-01").getTime(),
-          endDate: new Date("2023-07-31").getTime(),
-          timeoutMechanismType: CeremonyTimeoutType.FIXED,
-          penalty: 3600,
-          state: CeremonyState.OPENED,
-          type: CeremonyType.PHASE2,
-          coordinatorId: "uKm6XEjOKoeZUKAf2goY4vamgHE4",
-          lastUpdated: Date.now()
-        }
-      }
-    },
-    {
-      ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
-        data: {
-          title: "@zkecdsa-example",
-          prefix: "example",
-          description: "This is an example ceremony",
-          startDate: new Date("2023-07-01").getTime(),
-          endDate: new Date("2023-07-31").getTime(),
-          timeoutMechanismType: CeremonyTimeoutType.FIXED,
-          penalty: 3600,
-          state: CeremonyState.OPENED,
-          type: CeremonyType.PHASE2,
-          coordinatorId: "uKm6XEjOKoeZUKAf2goY4vamgHE4",
-          lastUpdated: Date.now()
-        }
-      }
-    },
-    {
-      ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
-        data: {
-          title: "test1",
-          prefix: "example",
-          description: "This is an example ceremony",
-          startDate: new Date("2023-07-01").getTime(),
-          endDate: new Date("2023-07-31").getTime(),
-          timeoutMechanismType: CeremonyTimeoutType.FIXED,
-          penalty: 3600,
-          state: CeremonyState.OPENED,
-          type: CeremonyType.PHASE2,
-          coordinatorId: "uKm6XEjOKoeZUKAf2goY4vamgHE4",
-          lastUpdated: Date.now()
-        }
-      }
-    },
-    {
-      ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
-        data: {
-          title: "test2",
-          prefix: "example",
-          description: "This is an example ceremony",
-          startDate: new Date("2023-07-01").getTime(),
-          endDate: new Date("2023-07-31").getTime(),
-          timeoutMechanismType: CeremonyTimeoutType.FIXED,
-          penalty: 3600,
-          state: CeremonyState.OPENED,
-          type: CeremonyType.PHASE2,
-          coordinatorId: "uKm6XEjOKoeZUKAf2goY4vamgHE4",
-          lastUpdated: Date.now()
-        }
-      }
-    },
-    {
-      ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
-        data: {
-          title: "test3",
-          prefix: "example",
-          description: "This is an example ceremony",
-          startDate: new Date("2023-07-01").getTime(),
-          endDate: new Date("2023-07-31").getTime(),
-          timeoutMechanismType: CeremonyTimeoutType.FIXED,
-          penalty: 3600,
-          state: CeremonyState.OPENED,
-          type: CeremonyType.PHASE2,
-          coordinatorId: "uKm6XEjOKoeZUKAf2goY4vamgHE4",
-          lastUpdated: Date.now()
-        }
-      }
-    },
-    {
-      ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
-        data: {
-          title: "test4",
-          prefix: "example",
-          description: "This is an example ceremony",
-          startDate: new Date("2023-07-01").getTime(),
-          endDate: new Date("2023-07-31").getTime(),
-          timeoutMechanismType: CeremonyTimeoutType.FIXED,
-          penalty: 3600,
-          state: CeremonyState.OPENED,
-          type: CeremonyType.PHASE2,
-          coordinatorId: "uKm6XEjOKoeZUKAf2goY4vamgHE4",
-          lastUpdated: Date.now()
-        }
-      }
-    },
-    {
-      ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
-        data: {
-          title: "test5",
-          prefix: "example",
-          description: "This is an example ceremony",
-          startDate: new Date("2023-07-01").getTime(),
-          endDate: new Date("2023-07-31").getTime(),
-          timeoutMechanismType: CeremonyTimeoutType.FIXED,
-          penalty: 3600,
-          state: CeremonyState.OPENED,
-          type: CeremonyType.PHASE2,
-          coordinatorId: "uKm6XEjOKoeZUKAf2goY4vamgHE4",
-          lastUpdated: Date.now()
-        }
-      }
-    },
-    {
-      ceremony: {
-        uid: "z37Z6PiCNPACp4gY9EMy",
-        data: {
-          title: "test6",
-          prefix: "example",
-          description: "This is an example ceremony",
-          startDate: new Date("2023-07-01").getTime(),
-          endDate: new Date("2023-07-31").getTime(),
-          timeoutMechanismType: CeremonyTimeoutType.FIXED,
-          penalty: 3600,
-          state: CeremonyState.OPENED,
-          type: CeremonyType.PHASE2,
-          coordinatorId: "uKm6XEjOKoeZUKAf2goY4vamgHE4",
-          lastUpdated: Date.now()
-        }
-      }
-    },
-
   ]);
 
   const [search, setSearch] = useState<string>("");
@@ -258,7 +78,24 @@ export const useInitialStateContext = () => {
     }
 
     fetchData()
-  })
+  },[])
 
   return { projects, setProjects, search, setSearch, loading, setLoading };
 };
+
+
+type StateProviderProps = {
+  children: React.ReactNode;
+};
+export const StateProvider: React.FC<StateProviderProps> = ({ children }) => {
+ 
+  const state =useInitialStateContext()
+
+  return (
+    <StateContext.Provider value={{...state }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);

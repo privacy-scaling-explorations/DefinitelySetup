6615fa742cecb2bb277e764b928669b763612572
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-08-31 15:25:56 +0100

    feat(contribution) add warning about changin tabs

--- web/src/pages/ProjectPage.tsx
+++ web/src/pages/ProjectPage.tsx
@@ -247,10 +247,6 @@ const ProjectPage: React.FC = () => {
                       <Text color="gray.500" fontSize={12} fontWeight="bold">
                         You have already contributed to this ceremony. Thank you for your participation.
                       </Text> :
-                      project.ceremony.data.state !== CeremonyState.OPENED ?
-                      <Text color="gray.500" fontSize={12} fontWeight="bold">
-                        This ceremony is {project.ceremony.data.state.toLocaleLowerCase()}.
-                      </Text> :
                       <>
                         <Text color="gray.500">
                           You can contribute to this project by running the CLI commands below.

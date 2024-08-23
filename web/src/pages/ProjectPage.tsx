55e231863becc844cb3f2bafba5ba6aac382b78a
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2024-08-23 17:22:33 +0100

    chore: cleanup (#128)

--- web/src/pages/ProjectPage.tsx
+++ web/src/pages/ProjectPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import {
   Box,
@@ -22,7 +22,6 @@ import {
   BreadcrumbItem,
   Flex,
   Icon,
-  Progress,
   SimpleGrid,
   SkeletonText,
   Table,
